69ce3be2ade95225d36db085cd8ca0c98db54815
Damian Doroba <damdor.dev@gmail.com>
2020-06-13 20:34:23 +0200

    build script (unity for linux is realy fucked up)

---
+++ Assets/Scripts/Editor/BuildScript.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildScript
+{
+
+    [MenuItem("Damdor/Build")]
+    public static void Build()
+    {
+        var a = new string[5];
+        var options = new BuildPlayerOptions
+        {
+            options = BuildOptions.None,
+            scenes = new[] { "Assets/Scenes/SampleScene.unity" },
+            target = BuildTarget.Android,
+            targetGroup = BuildTargetGroup.Android,
+            locationPathName = Path.Combine(Path.Combine(Path.GetDirectoryName(Application.dataPath), "Builds"), "ar.apk")
+        };
+        BuildPipeline.BuildPlayer(options);
+    }
+
+}
