4de4a511366709d891a8d0314a0da042f4c62a16
Damian Doroba <damdor.dev@gmail.com>
2020-06-13 19:00:26 +0200

    script to change graphic api (because unity on linux is fucked up)

---
+++ Assets/Scripts/Editor/SetAndroidGraphicApi.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEngine.Rendering;
+
+public class SetAndroidGraphicApi
+{
+
+    [MenuItem("Damdor/Set android graphic api")]
+    public static void Setup()
+    {
+        PlayerSettings.SetGraphicsAPIs(BuildTarget.Android,  new [] { GraphicsDeviceType.OpenGLES3 });
+    }
+    
+}
